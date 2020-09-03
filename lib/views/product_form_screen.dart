cc35f96a5cb0a29106ae9c74ff0faded0f86ce7b
Fabio Tavares <fabreder@gmail.com>
2020-09-03 18:13:10 -0300

    aula242

--- lib/views/product_form_screen.dart
+++ lib/views/product_form_screen.dart
@@ -6,23 +6,39 @@ class ProductFormScreen extends StatefulWidget {
 }
 
 class _ProductFormScreenState extends State<ProductFormScreen> {
+  //objeto para controlar o foco do campo preço
+  final _priceFocusNode = FocusNode();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Formulário Produto'),
       ),
-      body: Form(
-        child: ListView(
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(15.0),
-              child: TextFormField(
+      body: Padding(
+        padding: const EdgeInsets.all(15.0),
+        child: Form(
+          child: ListView(
+            children: [
+              TextFormField(
                 decoration: InputDecoration(labelText: 'Título'),
                 textInputAction: TextInputAction.next,
+                onFieldSubmitted: (_) {
+                  // executado quando o campo for submetido
+                  // passando o focus para o campo de preço
+                  FocusScope.of(context).requestFocus(_priceFocusNode);
+                },
               ),
-            ),
-          ],
+              TextFormField(
+                decoration: InputDecoration(labelText: 'Preço'),
+                textInputAction: TextInputAction.next,
+                focusNode: _priceFocusNode,
+                keyboardType: TextInputType.numberWithOptions(
+                  decimal: true,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
