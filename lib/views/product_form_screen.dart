cb5a3cc1d5c61eb1208eb7402ed6bf3f8fd579ca
Fabio Tavares <fabreder@gmail.com>
2020-09-03 19:56:29 -0300

    aula246

--- lib/views/product_form_screen.dart
+++ lib/views/product_form_screen.dart
@@ -1,4 +1,7 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:shop/providers/product.dart';
 
 class ProductFormScreen extends StatefulWidget {
   @override
@@ -11,18 +14,14 @@ class _ProductFormScreenState extends State<ProductFormScreen> {
   final _descriptionFocusNode = FocusNode();
   final _imageUrlFocusNode = FocusNode();
   final _imageUrlController = TextEditingController();
+  final _form = GlobalKey<FormState>();
+  final _formData = Map<String, Object>();
 
   @override
   void initState() {
     super.initState();
     // necessário para saber quando perdeu o foco e atualizar a imagem
     _imageUrlFocusNode.addListener(_updateImage);
-  }
-
-  void _updateImage() {
-    // setState vazio:
-    // isso pega os valores atuais das variáveis acima e aplica na tela
-    setState(() {});
   }
 
   @override
@@ -36,15 +35,49 @@ class _ProductFormScreenState extends State<ProductFormScreen> {
     _imageUrlFocusNode.dispose();
   }
 
+  void _updateImage() {
+    // setState vazio:
+    // isso pega os valores atuais das variáveis acima e aplica na tela
+    setState(() {});
+  }
+
+  void _saveForm() {
+    // chamada quando o formulário for submetido
+
+    // método save: chama o onSave de cada campo do formulário
+    _form.currentState.save();
+
+    // neste ponto eu tenho o map _formData com todos os dados digitados
+    // criando o objeto...
+    final newProduct = Product(
+      id: Random().nextDouble().toString(),
+      title: _formData['title'],
+      price: _formData['price'],
+      description: _formData['description'],
+      imageUrl: _formData['imageUrl'],
+    );
+
+    print(newProduct.id);
+    print(newProduct.title);
+    print(newProduct.price);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Formulário Produto'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.save),
+            onPressed: _saveForm,
+          ),
+        ],
       ),
       body: Padding(
         padding: const EdgeInsets.all(15.0),
         child: Form(
+          key: _form,
           child: ListView(
             children: [
               TextFormField(
@@ -55,6 +88,7 @@ class _ProductFormScreenState extends State<ProductFormScreen> {
                   // passando o focus para o campo de preço
                   FocusScope.of(context).requestFocus(_priceFocusNode);
                 },
+                onSaved: (value) => _formData['title'] = value,
               ),
               TextFormField(
                 decoration: InputDecoration(labelText: 'Preço'),
@@ -68,12 +102,14 @@ class _ProductFormScreenState extends State<ProductFormScreen> {
                   // passando o focus para o campo de preço
                   FocusScope.of(context).requestFocus(_descriptionFocusNode);
                 },
+                onSaved: (value) => _formData['price'] = double.parse(value),
               ),
               TextFormField(
                 decoration: InputDecoration(labelText: 'Descrição'),
                 maxLines: 3,
                 keyboardType: TextInputType.multiline,
                 focusNode: _descriptionFocusNode,
+                onSaved: (value) => _formData['description'] = value,
               ),
               Row(
                 crossAxisAlignment: CrossAxisAlignment.end,
@@ -88,6 +124,10 @@ class _ProductFormScreenState extends State<ProductFormScreen> {
                       // preciso aqui para ter acesso ao valor durante o
                       // controle de foco
                       controller: _imageUrlController,
+                      onFieldSubmitted: (_) {
+                        _saveForm();
+                      },
+                      onSaved: (value) => _formData['imageUrl'] = value,
                     ),
                   ),
                   Container(
