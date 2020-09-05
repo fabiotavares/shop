a6f9b3bd86c41e3414a939859209cd3a2c444ba2
Fabio Tavares <fabreder@gmail.com>
2020-09-05 17:21:12 -0300

    aula279 - FutureBuilder

--- lib/views/orders_screen.dart
+++ lib/views/orders_screen.dart
@@ -4,28 +4,9 @@ import 'package:shop/providers/orders.dart';
 import 'package:shop/widgets/app_drawer.dart';
 import 'package:shop/widgets/order_widget.dart';
 
-class OrdersScreen extends StatefulWidget {
-  @override
-  _OrdersScreenState createState() => _OrdersScreenState();
-}
-
-class _OrdersScreenState extends State<OrdersScreen> {
-  bool _isLoading = true;
-
-  @override
-  void initState() {
-    super.initState();
-    Provider.of<Orders>(context, listen: false).loadOrders().then((value) {
-      setState(() {
-        _isLoading = false;
-      });
-    });
-  }
-
+class OrdersScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final orders = Provider.of<Orders>(context);
-
     return Scaffold(
       appBar: AppBar(
         title: Text('Meus Pedidos'),
@@ -33,12 +14,32 @@ class _OrdersScreenState extends State<OrdersScreen> {
       drawer: AppDrawer(),
       body: RefreshIndicator(
         onRefresh: () => _refreshOrders(context),
-        child: _isLoading
-            ? Center(child: CircularProgressIndicator())
-            : ListView.builder(
-                itemCount: orders.itemsCount,
-                itemBuilder: (ctx, i) => OrderWidtget(orders.items[i]),
-              ),
+        child: FutureBuilder(
+          //solução elegante para se obter dados futuros
+          future: Provider.of<Orders>(context, listen: false).loadOrders(),
+          builder: (ctx, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              // ainda não terminou
+              return Center(child: CircularProgressIndicator());
+              //
+            } else if (snapshot.error != null) {
+              // um erro inesperado
+              return Center(child: Text('Ocorreu um erro!'));
+              //
+            } else {
+              // finalizou o processamento
+              return Consumer<Orders>(
+                // necessário usar o Consumer para obter o valor esperado
+                builder: (context, orders, child) {
+                  return ListView.builder(
+                    itemCount: orders.itemsCount,
+                    itemBuilder: (ctx, i) => OrderWidtget(orders.items[i]),
+                  );
+                },
+              );
+            }
+          },
+        ),
       ),
     );
   }
