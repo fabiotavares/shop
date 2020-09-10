ab04dc507f0e2b1ea6d8a5e03e40337e73d2f468
Fabio Tavares <fabreder@gmail.com>
2020-09-09 22:44:29 -0300

    aula313 - melhorando contraste das letras

--- lib/views/product_detail_screen.dart
+++ lib/views/product_detail_screen.dart
@@ -24,15 +24,30 @@ class ProductDetailScreen extends StatelessWidget {
             expandedHeight: 300,
             pinned: true,
             flexibleSpace: FlexibleSpaceBar(
-              title: Text(product.title),
-              background: Hero(
-                tag: product.id,
-                child: Image.network(
-                  product.imageUrl,
-                  fit: BoxFit.cover,
-                ),
-              ),
-            ),
+                title: Text(product.title),
+                background: Stack(
+                  fit: StackFit.expand,
+                  children: [
+                    Hero(
+                      tag: product.id,
+                      child: Image.network(
+                        product.imageUrl,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                    const DecoratedBox(
+                      decoration: BoxDecoration(
+                          gradient: LinearGradient(
+                        begin: Alignment(0, 0.8),
+                        end: Alignment(0, 0),
+                        colors: [
+                          Color.fromRGBO(0, 0, 0, 0.6),
+                          Color.fromRGBO(0, 0, 0, 0),
+                        ],
+                      )),
+                    ),
+                  ],
+                )),
           ),
           SliverList(
             delegate: SliverChildListDelegate([
@@ -51,6 +66,7 @@ class ProductDetailScreen extends StatelessWidget {
                   textAlign: TextAlign.center,
                 ),
               ),
+              SizedBox(height: 1000),
             ]),
           ),
         ],
